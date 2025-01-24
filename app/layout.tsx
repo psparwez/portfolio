23ee418f70fd4d498a26d28d5ef397ffb09f9a01
PS Parwez <infoparwez200@gmail.com>
2025-01-24 19:39:51 +0530

    fix: set default theme to Dark ✔

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="data-theme"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
